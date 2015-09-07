d7bf674fb34ef727ddaf375359cbb2bf35b7b016
baowei.yao <baowei.yao@hypers.com>
2015-09-08 01:50:28 +0800

    bug fix..

--- lib/puppet/parser/functions/path_to_array.rb
+++ lib/puppet/parser/functions/path_to_array.rb
@@ -1,21 +1,47 @@
+#
+# path_to_array.rb
+#
 module Puppet::Parser::Functions
-  newfunction(:path_to_array, :type => :rvalue) do |args|
+  newfunction(:path_to_array, :type => :rvalue, :doc => <<-EOS
+This function split path into a array.
 
-    res = args[0]
-    
+*Examples:*
+
+   path_to_array('/aa/bb/cc/dd')
+     Would result in: ['/aa/bb/cc/dd']
+
+   path_to_array('/aa/bb/cc/dd', 1)
+     Would result in: ['/aa/bb/cc/dd', '/aa/bb/cc']
+
+   path_to_array('/aa/bb/cc/dd', 2)
+     Would result in: ['/aa/bb/cc/dd', '/aa/bb/cc', '/aa/bb']
+    EOS
+
+  ) do |args|
+
+    raise(Puppet::ParseError, "path_to_array(): Wrong number of arguments " +
+      "given (#{args.size} for 1)") if args.size < 1
+
     array = []
     array.push(args[0])
 
-    if args.size == 1 
+    if args.size == 2 
+      res = args[0]
+      count_path = 0
       while res != '/' do
         res = File.dirname(res)
-        array.push(res) if res != '/'
+        if res != '/'
+          count_path = count_path + 1
+        end
       end
-    elsif args.size == 2 
-      e_count = args[1] - 1
-      e_count.times do
-        res = File.dirname(res)
-        array.push(res) if res != '/'
+      if args[1] > count_path 
+        raise(Puppet::ParseError, "path_to_array(): invalid argument (#{args[1]})")
+      else 
+        res = args[0]
+        args[1].times do
+          res  = File.dirname(res)
+          array.push(res) if res != '/'
+        end
       end
     end
 
